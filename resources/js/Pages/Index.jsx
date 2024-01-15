9b4ca8d551beb5e59fffc0bfb9927919bd212e5d
Denis M Jethro <denisjethro@gmail.com>
2024-01-15 22:51:08 +0700

    First-Commit Laravel + InertiaJs + ViteJs + ReactJs + Tailwindcss

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Index(){
+    return(
+        <div className="text-xl text-red-600">
+            Hello World!
+        </div>
+    )
+};
+
+export default Index;
