0570486de613c40eafab94500bd5ca266a05aa15
Bellanie13 <92662128+Bellanie13@users.noreply.github.com>
2024-02-01 15:41:29 +0700

    Update UserController.php
    ini user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,38 +4,73 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $users = User::all();
+        $data = [
+            'users' => $users,
+        ];
+
+        return Inertia::render('UserIndex', $data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(Request $request)
+    public function create()
     {
-           
+        return Inertia::render('UserCreate');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+    public function store(Request $request)
+    {
+        // Validate the incoming request data
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required|min:6',
+            'role' => 'required|in:admin,direksi,humas,dewan',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'role' => $request->role,
+        ]);
+        return redirect()->route('user.index')->with('success', 'User berhasil dibuat');
+    }
+
+    public function edit(User $user)
+    {
+        return Inertia::render('UserEdit', ['user' => $user]);
+    }
+
     public function update(Request $request, User $user)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email,' . $user->id,
+            'role' => 'required|in:admin,direksi,humas,dewan',
+        ]);
+
+        // Update the user
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => $request->role,
+        ]);
+
+        return redirect()->route('user.index')->with('success', 'User berhasil diperbaharui');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(User $user)
     {
-        //
+        // Delete the user
+        $user->delete();
+        
+        return redirect()->route('user.index')->with('success', 'User berhasil dihapus');
     }
 }
+
