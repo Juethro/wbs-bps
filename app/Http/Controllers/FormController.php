5b4f4c79aa09a2b87667505b9f4e12d127e3b9e3
Denis M Jethro <denisjethro@gmail.com>
2024-02-01 09:14:28 +0700

    Template FormController

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -7,13 +7,43 @@ use Inertia\Inertia;
 
 class FormController extends Controller
 {
-    function Index()
+    function home()
     {
         return Inertia::render('FormPengaduan');
     }
 
-    function Status()
+    function pengaduan()
+    {
+        return Inertia::render('FormPengaduan');
+    }
+
+    function status()
     {
         return Inertia::render('FormStatus');
+    }
+
+    function track(Request $request)
+    {
+        /**
+         * - Validasi Request
+         * - Return Data dari Database
+         */
+    }
+
+    function submitStore(Request $request)
+    {
+        /**
+         * - Validasi Request
+         * - Simpan file dan path file tersebut 
+         */
+        
+    }
+
+    function submitRevision(Request $request)
+    {
+        /**
+         * - Validasi Request
+         * - Update database
+         */
     }
 }
