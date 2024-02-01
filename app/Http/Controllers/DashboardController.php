5394486d9fb5e72519a665f3556ce3dac58d8579
Denis M Jethro <denisjethro@gmail.com>
2024-02-01 09:16:24 +0700

    Dashboard Action Update + Routes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,26 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\pengaduan;
+use App\Models\status_history;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Validator;
 
 class DashboardController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display Dashboard Each Roles
      */
-    public function index()
+    public function admin()
     {
         return Inertia::render('Dashboard'); 
     }
 
-    public function humas()
+    public function kurator()
     {
-        dd('ini dashboard humas');
+        dd('ini dashboard kurator');
     }
 
-    public function direksi()
+    public function validator()
     {
-        dd('ini dashboard direksi');
+        dd('ini dashboard validator');
     }
 
     public function dewan()
@@ -30,4 +33,93 @@ class DashboardController extends Controller
         dd('ini dashboard dewan');
     }
 
+    /**
+     * Validator Utilities
+     */
+
+    public function approveLaporanValidator($ticket)
+    {
+        /**
+         * - Mengubah review di Database
+        * - Mail Notifikasi
+        * - Simpan Histori Review
+        */
+    }
+
+    public function editLaporan(Request $request, $ticket)
+    {
+        /**
+         * - Validasi Request
+         * - Membuat Mail Notifikasi menggunakan Request (Instruksi dari validator)
+         * - Update status editable
+         * - Simpan Histori Review
+         */
+
+
+    }
+
+
+    /** 
+     * Kurator Utilities
+     */
+
+    public function approveLaporanKurator(Request $request, $ticket)
+    {
+        /**
+         * - Validasi Request
+         * - Mengubah review di database
+         * - Menyimpan Multiple File + simpan catatan
+         * - Simpan Histori review
+         * - Email Notifikasi
+         */
+    }
+
+    public function deniedLaporanKurator($ticket)
+    {
+        /**
+         * - Validasi Request
+         * - Mengubah review di database
+         * - Email Notifikasi
+         */
+    }
+
+    /**
+     * Dewan Utilities
+     */
+
+    public function approveLaporanDewan(Request $request, $ticket)
+    {
+        /**
+         * - Validasi Request
+         * - Mengubah review di database
+         * - Menyimpan Multiple File
+         * - Simpan Histori Review
+         * - Email Notifikasi
+         */
+    }
+
+    public function beyondOurJurisdiction($ticket)
+    {
+        /**
+         * - Mengubah review di Database
+         * - Mail Notifikasi
+         * - Simpan Histori Review
+         */
+    }
+
+    public function deniedLaporanDewan(Request $request, $ticket)
+    {
+        /**
+         * - Mengubah review di Database
+         * - Mail Notifikasi
+         * - Simpan Histori Review
+         */
+
+        
+    }
+
+    public function continue(Request $request, $ticket)
+    {
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,21 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EmailController;
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 // Form Pelaporan & Tracking Status
-Route::get('/', [FormController::class, 'Index'])->name('home');
-Route::get('/status', [FormController::class, 'Status'])->name('status');
+Route::get('/', [FormController::class, 'home'])->name('home');
+Route::get('/status', [FormController::class, 'status'])->name('status');
+Route::get('/pengaduan', [FormController::class, 'pengaduan'])->name('pengaduan');
+
+Route::post('/status/track', [FormController::class, 'track'])->name('status.track');
+Route::post('/pengaduan/submit', [FormController::class, 'submitStore'])->name('pengaduan.submit');
+Route::post('/pengaduan/submitrevisi', [FormController::class, 'submitRevision'])->name('pengaduan.revision');
 
 // Login
 Route::get('/login', [LoginController::class, 'showLogin']);
@@ -19,29 +26,52 @@ Route::get('/cek', [LoginController::class, 'check']);
 // Dashboard
 Route::group(['prefix' => 'dashboard'], function () {
 
+
     // Dashboard Admin
     Route::middleware(['auth', 'onlyAdmin'])->group(function () {
-        Route::get('/admin', [DashboardController::class, 'index'])->name('dashboard.admin');
-
+        Route::get('/admin', [DashboardController::class, 'admin'])->name('dashboard.admin');
+        Route::get('/admin/status/administratif', [DashboardController::class, 'statusAdministratif'])->name('admin.status.administratif');
+        Route::get('/admin/status/teknis', [DashboardController::class, 'statusTeknis'])->name('admin.status.teknis');
+        Route::get('/admin/user', [DashboardController::class, 'user'])->name('admin.user');
+        Route::get('/admin/email', [DashboardController::class, 'email'])->name('admin.email');
 
         Route::post('/admin/user/new', [UserController::class, 'create'])->name('user.create');
         Route::patch('/admin/user/update', [UserController::class], 'update')->name('user.update');
         Route::delete('/admin/user/delete', [UserController::class], 'destroy')->name('user.delete');
-    });
-    
-    // Dashboard Humas
-    Route::middleware(['auth', 'onlyHumas'])->group(function () {
-        Route::get('/humas', [DashboardController::class, 'humas'])->name('dashboard.humas');
+
+        Route::post('/admin/email/new', [EmailController::class, 'create'])->name('email.create');
+        Route::patch('/admin/email/update', [EmailController::class, 'update'])->name('email.update');
+        Route::delete('/admin/email/delete', [EmailController::class, 'delete'])->name('email.delete');
+
     });
 
-    // Dashboard Direksi
+
+    // Dashboard Tim Validator
     Route::middleware(['auth', 'onlyDireksi'])->group(function () {
-        Route::get('/direksi', [DashboardController::class, 'direksi'])->name('dashboard.direksi');
+        Route::get('/validator', [DashboardController::class, 'validator'])->name('dashboard.validator');
+
+        Route::post('/validator/approve/{ticket}', [DashboardController::class, 'approveLaporanValidator'])->name('validator.approve');
+        Route::post('/validator/edit/{ticket}', [DashboardController::class, 'editLaporan'])->name('validator.editable');
     });
+
+
+    // Dashboard Tim Kurator
+    Route::middleware(['auth', 'onlyHumas'])->group(function () {
+        Route::get('/kurator', [DashboardController::class, 'kurator'])->name('dashboard.kurator');
+
+        Route::post('/kurator/approve/{ticket}', [DashboardController::class, 'approveLaporanKurator'])->name('kurator.approve');
+        Route::post('/kurator/denied/{ticket}', [DashboardController::class, 'deniedLaporanKurator'])->name('kurator.denied');
+    });
+
 
     // Dashboard Dewan
     Route::middleware(['auth', 'onlyDewan'])->group(function () {
         Route::get('/dewan', [DashboardController::class, 'dewan'])->name('dashboard.dewan');
+
+        Route::post('/dewan/approve/{ticket}', [DashboardController::class, 'approveLaporanDewan'])->name('dewan.approve');
+        Route::post('/dewan/denied/{ticket}', [DashboardController::class, 'deniedLaporanDewan'])->name('dewan.denied');
+        Route::post('/dewan/beyond/{ticket}', [DashboardController::class, 'beyondOurJurisdiction'])->name('dewan.beyondOurJurisdiction');
+        Route::post('/dewan/continue/{ticket}', [DashboardController::class, ''])->name('dewan.continue');
     });
 
 });
